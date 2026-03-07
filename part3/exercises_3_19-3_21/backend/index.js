cbc6cbf3213988af8a4c41e595f5c3f0b7e1e7af
imarag <giannis.marar@hotmail.com>
2026-03-07 03:20:44 +0200

    change

--- part3/exercises_3_19-3_21/backend/index.js
+++ part3/exercises_3_19-3_21/backend/index.js
@@ -4,12 +4,13 @@ const morgan = require('morgan')
 const Person = require('./models/person')
 
 const app = express()
-
 app.use(express.static('dist'))
+
 app.use(express.json())
 
 morgan.token("body", (req, res) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
 
 app.get("/api/persons", (request, response) => {
     Person.find({})
