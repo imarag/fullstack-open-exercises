af2d78db62117ef8ca0de80f1b3b264b138f71cf
imarag <giannis.marar@hotmail.com>
2026-04-01 20:19:10 +0300

    delete unwanted

--- part7/exercises_7_10-7_13/backend/index.js
+++
@@ -1,7 +0,0 @@
-const app = require('./app')
-const { PORT } = require('./utils/config')
-const logger = require('./utils/logger')
-
-app.listen(PORT, () => {
-    logger.info(`Server running on port ${PORT}`)
-})
